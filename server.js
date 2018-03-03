01cfdc5e44af2b96301172ffe02e990345cccb0b
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-03 22:59:13 +0500

    node deployment fixes

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.use(express.static(__dirname + '/dist'));
+
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 3000,function(){
     console.log('server listening on port :' + process.env.PORT || 3000);
