f2b0e70cee4a803d0ce08a7f0a6a7f5f28b3a894
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-03 23:15:43 +0500

    code deployment fixes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 
-app.get('*', function(req, res) {
-
-    res.sendFile(path.join(__dirname + 'dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+    
+//     res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 app.listen(process.env.PORT || 3000,function(){
     console.log('server listening on port :' + process.env.PORT || 3000);
