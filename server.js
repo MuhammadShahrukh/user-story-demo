46ec9715fb16d37de5c357d86bb4410eaf011674
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-03 23:58:45 +0500

    fixes

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var app = express();
 app.use(express.static( path.join(__dirname , 'dist') ));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    console.log( __dirname );
+    console.log( path.join(__dirname, 'dist/index.html') );
+    
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 3000,function(){
