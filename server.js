4d0558417099a64890c2ee3b15c1a9ebf74dbe67
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-04 00:22:52 +0500

    code deployment fixes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static( path.join('./dist') ));
+app.use(express.static( __dirname+ '/dist' ));
 
 app.get('*', (req, res) => {
  
