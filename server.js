e29bd5c05cdc9cc2103a1587d1f73c5cdbc830ca
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-03 23:34:43 +0500

    deployment code fixes

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static( path.join(__dirname , 'dist') ));
 
-
-// app.get('/', function(req, res) {
-//     console.log( __dirname );
-//     console.log( path.join(__dirname + '/dist/index.html') )
-
-//     res.sendFile(path.join(__dirname + '/dist/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
 
 app.listen(process.env.PORT || 3000,function(){
-    console.log('server listening on port :' + process.env.PORT || 3000);
+    console.log('server listening on port :' + (process.env.PORT || 3000));
 })
