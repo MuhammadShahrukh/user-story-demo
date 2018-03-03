049ad3246b6e3762a9d7654fe0e547faba77c648
Muhammad Shahrukh Khan <developer_shahrukh@hotmail.com>
2018-03-03 21:36:50 +0500

    port updated

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(PORT,function(){
-    console.log('server listening on port :' + PORT );
+    console.log('server listening on port :' + process.env.PORT || 3000);
 })
